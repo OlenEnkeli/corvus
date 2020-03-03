5029ca40ec2a8b86e3aa2268f4ff81bb46310a2e
Антон Никольский <angeloffree@yandex.ru>
2020-03-03 17:37:27 +0300

    Up release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='corvus',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/OlenEnkeli/corvus',
     packages=find_packages(),
     license='BSD',
